d1897dd7b1fda8dc6fa78b4f924eb622ae382ddd
Andres Rodriguez <andres.rodriguez0215@gmail.com>
2018-05-04 17:57:28 -0500

    add props

--- dev/js/components/KeyBoard.jsx
+++ dev/js/components/KeyBoard.jsx
@@ -7,15 +7,19 @@ import nextLevel from '../simon.js';
 class Keyboard extends Component{
     constructor(props) {
         super(props);
+        this.arrTotal = new Array();
     }
     componentDidMount(){
         nextLevel(0);
-        this.initRowOne();
+        console.log(this.initRowOne());
+        console.log(this.initRowTwo());
+        console.log(this.initRowThree());
+        console.log(this.arrTotal)
     }
     render() {
         return(
             <div className="keyboard">
-                <RowOne/>
+                <RowOne keys={this.initRowOne()}/>
                 <RowTwo/>
                 <RowThree/>
             </div>
@@ -23,18 +27,71 @@ class Keyboard extends Component{
     }
 
     initRowOne() {
-        let arr = new Array();
-        arr.push(this.nobj('81', 'q'));
-        arr.push(this.nobj('87', 'w'));
-        arr.push(this.nobj('69', 'e'));
-        arr.push(this.nobj('82', 'r'));
-        arr.push(this.nobj('84', 't'));
-        arr.push(this.nobj('89', 'y'));
-        arr.push(this.nobj('85', 'u'));
-        arr.push(this.nobj('73', 'i'));
-        arr.push(this.nobj('79', 'o'));
-        arr.push(this.nobj('80', 'p'));
-        console.log(arr);
+       let arrOne = new Array();
+       this.arrTotal.push(this.nobj('81', 'q'));
+       this.arrTotal.push(this.nobj('87', 'w'));
+       this.arrTotal.push(this.nobj('69', 'e'));
+       this.arrTotal.push(this.nobj('82', 'r'));
+       this.arrTotal.push(this.nobj('84', 't'));
+       this.arrTotal.push(this.nobj('89', 'y'));
+       this.arrTotal.push(this.nobj('85', 'u'));
+       this.arrTotal.push(this.nobj('73', 'i'));
+       this.arrTotal.push(this.nobj('79', 'o'));
+       this.arrTotal.push(this.nobj('80', 'p'));
+       arrOne.push(this.nobj('81', 'q'));
+       arrOne.push(this.nobj('87', 'w'));
+       arrOne.push(this.nobj('69', 'e'));
+       arrOne.push(this.nobj('82', 'r'));
+       arrOne.push(this.nobj('84', 't'));
+       arrOne.push(this.nobj('89', 'y'));
+       arrOne.push(this.nobj('85', 'u'));
+       arrOne.push(this.nobj('73', 'i'));
+       arrOne.push(this.nobj('79', 'o'));
+       arrOne.push(this.nobj('80', 'p'));
+       return arrOne;
+    }
+    initRowTwo() {
+        
+        let arrTwo = new Array();
+       this.arrTotal.push(this.nobj('65', 'a'));
+       this.arrTotal.push(this.nobj('83', 's'));
+       this.arrTotal.push(this.nobj('68', 'd'));
+       this.arrTotal.push(this.nobj('70', 'f'));
+       this.arrTotal.push(this.nobj('71', 'g'));
+       this.arrTotal.push(this.nobj('72', 'h'));
+       this.arrTotal.push(this.nobj('74', 'j'));
+       this.arrTotal.push(this.nobj('75', 'k'));
+       this.arrTotal.push(this.nobj('76', 'l'));
+       
+       arrTwo.push(this.nobj('65', 'a'));
+       arrTwo.push(this.nobj('83', 's'));
+       arrTwo.push(this.nobj('68', 'd'));
+       arrTwo.push(this.nobj('70', 'f'));
+       arrTwo.push(this.nobj('71', 'g'));
+       arrTwo.push(this.nobj('72', 'h'));
+       arrTwo.push(this.nobj('74', 'j'));
+       arrTwo.push(this.nobj('75', 'k'));
+       arrTwo.push(this.nobj('76', 'l'));
+       return arrTwo;
+    }
+    initRowThree() {
+        let arrThree = new Array();
+       this.arrTotal.push(this.nobj('90', 'z'));
+       this.arrTotal.push(this.nobj('88', 'x'));
+       this.arrTotal.push(this.nobj('67', 'c'));
+       this.arrTotal.push(this.nobj('86', 'v'));
+       this.arrTotal.push(this.nobj('66', 'b'));
+       this.arrTotal.push(this.nobj('78', 'n'));
+       this.arrTotal.push(this.nobj('77', 'm'));
+
+       arrThree.push(this.nobj('90', 'z'));
+       arrThree.push(this.nobj('88', 'x'));
+       arrThree.push(this.nobj('67', 'c'));
+       arrThree.push(this.nobj('86', 'v'));
+       arrThree.push(this.nobj('66', 'b'));
+       arrThree.push(this.nobj('78', 'n'));
+       arrThree.push(this.nobj('77', 'm'));
+       return arrThree;
     }
     nobj(key, value) {
         let obj = {
--- dev/js/components/RowOne.jsx
+++ dev/js/components/RowOne.jsx
@@ -8,16 +8,16 @@ class RowOne extends Component{
     render(){
         return(
             <div className="row">
-                <div className="key" data-key="81">q</div>
-                <div className="key" data-key="87">w</div>
-                <div className="key" data-key="69">e</div>
-                <div className="key" data-key="82">r</div>
-                <div className="key" data-key="84">t</div>
-                <div className="key" data-key="89">y</div>
-                <div className="key" data-key="85">u</div>
-                <div className="key" data-key="73">i</div>
-                <div className="key" data-key="79">o</div>
-                <div className="key" data-key="80">p</div>
+            {this.props.keys.map((key)=>{
+                console.log(key.value);
+                <div 
+                    id={key}
+                    className="key" 
+                    data-key={key}>
+                    {key.value}
+                </div>
+
+            })}
             </div>
         );
     }
